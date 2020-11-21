1d197459b8052409e56004463aa5ac921253058a
borsuk664 <eloelo1@o2.pl>
2020-11-21 02:01:42 +0100

    working alpha

--- Launcher/MainWindow.xaml.cs
+++ Launcher/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ using System.Windows.Forms;
 using System.Threading;
 using System.ComponentModel;
 using System.Reflection.Emit;
+using System.IO.Compression;
 
 namespace Launcher
 {
@@ -33,12 +34,16 @@ namespace Launcher
         public MainWindow()
         {
             InitializeComponent();
+            check_ver();
+        }
+        public void check_ver()
+        {
             var exePath = AppDomain.CurrentDomain.BaseDirectory;
             var j = new StreamReader(exePath + "version.json");
             var j1 = j.ReadToEnd();
             ver_actual jj = JsonConvert.DeserializeObject<ver_actual>(j1);
-            Console.WriteLine("Aktualna: " + jj.version);
             textblock.Text = "Version: " + jj.version;
+            j.Close();
             Check_Update();
         }
         public class ver
@@ -54,29 +59,42 @@ namespace Launcher
         {
             var exePath = AppDomain.CurrentDomain.BaseDirectory;
             Console.WriteLine(exePath);
-            Process.Start(exePath + "Launcher.exe");
+            var exefPath = exePath + "build\\";
+            Process.Start(exefPath + "mmo.exe");
         }
         public void Check_Update()
         {
+            label.Text = "Checking for updates...";
             var exePath = AppDomain.CurrentDomain.BaseDirectory;
             var json = new WebClient().DownloadString("http://127.0.0.1/files/server.json");
             var j = new StreamReader(exePath + "version.json");
             var j1 = j.ReadToEnd();
+            j.Close();
             ver_actual jj = JsonConvert.DeserializeObject<ver_actual>(j1);
             ver Server = JsonConvert.DeserializeObject<ver>(json);
             if (jj.version == Server.version)
             {
                 Button_Play.IsEnabled = true;
+                label.Text = "Ready to play!";
+                progressBar.Opacity = 0;
             }
             else
             {
-                var Uri = "http://127.0.0.1/files/Aslains_WoT_Modpack_Installer_v.1.10.0.2_02.exe";
+                ///var Uri = "http://127.0.0.1/files/Aslains_WoT_Modpack_Installer_v.1.10.0.2_02.exe";
                 Button_Play.IsEnabled = false;
-                WebClient webClient = new WebClient();
-                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
-                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
-                webClient.DownloadFileAsync(new Uri(Uri), exePath + "myfile.exe");
+                Dl();
+
             }
+        }
+
+        public void Dl()
+        {
+            var Uri = "http://127.0.0.1/files/build.zip";
+            var exePath = AppDomain.CurrentDomain.BaseDirectory;
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFileAsync(new Uri(Uri), exePath + "build.zip");
         }
         private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
@@ -86,7 +104,48 @@ namespace Launcher
         private void Completed(object sender, EventArgs e)
         {
             label.Text = "Download completed!";
-            Button_Play.IsEnabled = true;
+            ///Button_Play.IsEnabled = true;
+            extract();
         }
-    }    
+        public void extract()
+        {
+            label.Text = "Extracting update";
+            var exePath = AppDomain.CurrentDomain.BaseDirectory;
+            string path = exePath + "build";
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+            finally
+            {
+                ZipFile.ExtractToDirectory(exePath + "build.zip", exePath);
+                File.Delete(exePath + "build.zip");
+                
+                var json = new WebClient().DownloadString("http://127.0.0.1/files/server.json");
+                Console.WriteLine(json);
+
+                //File.Delete(exePath + "version.json");
+                //using (StreamWriter sw = File.CreateText(exePath))
+                //{
+                //    sw.WriteLine(json);
+                //}
+                var j = new StreamReader(exePath + "version.json");
+                var j1 = j.ReadToEnd();
+                ver_actual jj = JsonConvert.DeserializeObject<ver_actual>(j1);
+                Console.WriteLine(j);
+                j.Close();
+                string text = File.ReadAllText("version.json");
+                text = text.Replace((string)j1, (string)json);
+                File.WriteAllText("version.json", text);
+                label.Text = "Ready to play!";
+                Button_Play.IsEnabled = true;
+                progressBar.Opacity = 0;
+                check_ver();
+            }
+        }
+
+    }
 }
